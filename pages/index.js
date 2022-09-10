2610bd635965596bc61dfe4e2641dfd2eca00f42
jinah92 <jinah0818@gmail.com>
2022-09-10 15:33:38 +0900

    setting server side generation

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+import fs from 'fs/promises'
+import path from 'path'
+
+function HomePage(props) {
+  const { products } = props
+
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
+  )
+}
+export async function getStaticProps(context) {
+  console.log('regenerating...')
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
+  const json = await fs.readFile(filePath)
+  const products = JSON.parse(json).products
+
+  if (!products) {
+    return {
+      redirect: {
+        destination: '/no-data'
+      }
+    }
+  }
+
+  if (products.length === 0) {
+    return { notFound: true }
+  }
+
+  return {
+    props: {
+      products
+    },
+    revalidate: 10
+  }
+}
+
+export default HomePage
