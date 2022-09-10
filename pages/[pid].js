f63a49e4a1161dc9e5618e80c9d925eba44cbc5f
jinah92 <jinah0818@gmail.com>
2022-09-10 15:58:53 +0900

    setting getStaticPath & getStaticProps for dynamic page

---
+++ pages/[pid].js
@@ -0,0 +1,39 @@
+import fs from 'fs/promises'
+import path from 'path'
+import { Fragment } from 'react'
+
+function ProductDetailPage(props) {
+  const { product } = props
+  return (
+    <Fragment>
+      <h1>{product.title}</h1>
+      <p>{product.description}</p>
+    </Fragment>
+  )
+}
+
+export async function getStaticProps(context) {
+  const { params } = context
+  const productId = params.pid
+
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
+  const json = await fs.readFile(filePath)
+  const products = JSON.parse(json).products
+
+  const product = products.find((product) => product.id === productId)
+
+  return {
+    props: {
+      product
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { pid: 'p1' } }, { params: { pid: 'p2' } }, { params: { pid: 'p3' } }],
+    fallback: false
+  }
+}
+
+export default ProductDetailPage
